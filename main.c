4a86977ecf49a447b239e2ae1cbc64e3e51c0d8b
mrrajon04 <maqsudurrahman@u.boisestate.edu>
2025-02-12 12:47:11 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "../src/lab.h"
 
 void parse_args(int argc, char **argv) {
@@ -19,6 +21,19 @@ void parse_args(int argc, char **argv) {
 
 int main(int argc, char **argv) {
     parse_args(argc, argv);
-    // Other initialization and the main loop of your shell here
+
+    // Read input from the user using GNU Readline
+    char *input;
+    while ((input = readline("shell> ")) != NULL) {
+        // Add the input to the history
+        add_history(input);
+
+        // Process the input (for now, just print it)
+        printf("You entered: %s\n", input);
+
+        // Free the allocated input
+        free(input);
+    }
+
     return 0;
 }
